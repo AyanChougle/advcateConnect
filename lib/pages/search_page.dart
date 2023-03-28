e601705174827447f5cbf8d708cb68763c8558d7
Ziyad Surve <zsurve16@gmail.com>
2023-03-28 12:32:02 +0530

    Ui has been fixed on Home view

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -66,12 +66,11 @@ class _SearchPageState extends State<SearchPage> {
       backgroundColor: styles.bgcolor,
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                height: 20,
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -100,13 +99,9 @@ class _SearchPageState extends State<SearchPage> {
                 thickness: 0.6,
                 color: Colors.grey[400],
               ),
-              Row(
-                children: [
-                  Text(
-                    'What are\n you looking for ?',
-                    style: styles.headLineStyle2.copyWith(fontSize: 25),
-                  ),
-                ],
+              Text(
+                'What are\nyou looking for ?',
+                style: styles.headLineStyle2.copyWith(fontSize: 25),
               ),
               const SizedBox(
                 height: 25,
@@ -127,22 +122,14 @@ class _SearchPageState extends State<SearchPage> {
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 15,
+              const SizedBox(height: 10),
+              const Text(
+                'Suggested Lawyers : ',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-              Row(
-                children: const [
-                  Text(
-                    'Suggested Lawyers : ',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 5),
               ListView.builder(
                 itemCount: advocateList.length,
                 shrinkWrap: true,
@@ -150,13 +137,12 @@ class _SearchPageState extends State<SearchPage> {
                 itemBuilder: (_, index) {
                   return Container(
                     margin: const EdgeInsets.symmetric(vertical: 10),
-                    height: 250,
+                    // height: 200,
                     decoration: BoxDecoration(
                       color: Colors.black,
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         ClipRRect(
                           borderRadius: const BorderRadius.only(
@@ -165,104 +151,93 @@ class _SearchPageState extends State<SearchPage> {
                           child: Image.asset(
                             advocateList[index].imageUrl,
                             fit: BoxFit.cover,
-                            height: 250,
-                            width: 170,
+                            height: 200,
+                            width: 150,
                           ),
                         ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  advocateList[index].name,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  advocateList[index].description,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                                Text(
-                                  advocateList[index].type,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  advocateList[index].exp,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  advocateList[index].price,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 15,
-                                ),
-                                RatingBar.builder(
-                                  ignoreGestures: true,
-                                  unratedColor: Colors.white,
-                                  itemSize: 18,
-                                  initialRating: 4,
-                                  minRating: 1,
-                                  direction: Axis.horizontal,
-                                  allowHalfRating: true,
-                                  itemCount: 5,
-                                  itemPadding: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  itemBuilder: (context, _) => const Icon(
-                                    Icons.star,
-                                    color: Colors.amber,
-                                  ),
-                                  onRatingUpdate: (rating) {},
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                TextButton(
-                                  style: TextButton.styleFrom(
-                                    primary: Colors.white,
-                                    backgroundColor: Colors.blue,
-                                  ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Booking(),
-                                      ),
-                                    );
-                                  },
-                                  child: const Text(
-                                    'Book Appointment',
-                                  ),
-                                ),
-                              ],
+                        const SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              advocateList[index].name,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
                             ),
-                          ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              advocateList[index].description,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              advocateList[index].type,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              advocateList[index].exp,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              advocateList[index].price,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            RatingBar.builder(
+                              ignoreGestures: true,
+                              unratedColor: Colors.white,
+                              itemSize: 18,
+                              initialRating: 4,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemPadding:
+                                  const EdgeInsets.symmetric(horizontal: 4.0),
+                              itemBuilder: (context, _) => const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              onRatingUpdate: (rating) {},
+                            ),
+                            const SizedBox(height: 5),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: Colors.blue,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Booking(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Book Appointment',
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
